72cb7b2b6ea08bfc06b0884d08e19be4869e7838
aderose <aderose@hex.otago.ac.nz>
2018-10-01 21:54:27 +1300

    done

--- other/P3/lab23h/queue-array.c
+++ other/P3/lab23h/queue-array.c
@@ -3,58 +3,61 @@
 #include "mylib.h"
 #include "queue.h"
 struct queue {
-  int num_items;
-  int head;
-  int capacity;
-  double *items;
+    int num_items;
+    int head;
+    int capacity;
+    double *items;
 };
 queue queue_new() {
-  int default_size = 7;
-  queue q = emalloc(sizeof *q);
-  q->num_items = 0;
-  q->head = 0;
-  q->capacity = default_size;
-  q->items = emalloc(sizeof(q->items[0]) * default_size);
-  return q;
+    int default_size = 7;
+    queue q = emalloc(sizeof *q);
+    q->num_items = 0;
+    q->head = 0;
+    q->capacity = default_size;
+    q->items = calloc(default_size, sizeof(q->items[0]));
+    /*for (i = 0; i < default_size; i++) {
+    //    q->items[i] = 0.0;
+    }*/
+    return q;
 }
 queue queue_free(queue q) {
-  free(q->items);
-  free(q);
-  return NULL;
+    free(q->items);
+    free(q);
+    return NULL;
 }
 void enqueue(queue q, double item) {
-  if (q->num_items < q->capacity) {
-    q->items[(q->head + q->num_items++) % q->capacity] = item;
-  }
+    if (q->num_items < q->capacity) {
+        q->items[(q->head + q->num_items++) % q->capacity] = item;
+    }
 }
 double dequeue(queue q) {
-  double result = -1;
-  if (q->num_items > 0) {
-    result = q->items[q->head++];
-    q->num_items--;
-  }
-  return result;
+    double result = -1;
+    if (q->num_items > 0) {
+        result = q->items[q->head++];
+        q->num_items--;
+    }
+    return result;
 }
 void queue_print(queue q) {
-  /* print queue contents one per line to 2 decimal places */
-  int i = 0, head = q->head;
-  while (i<q->num_items) {
-    printf("%.2f\n", q->items[q->head++%q->capacity]);
-    i++;
-  }
-  q->head = head;
+    /* print queue contents one per line to 2 decimal places */
+    int i = 0, head = q->head;
+    while (i<q->num_items) {
+        printf("%.2f\n", q->items[q->head++%q->capacity]);
+        i++;
+    }
+    q->head = head;
 }
 void queue_print_info(queue q) {
-  int i;
-  printf("capacity %d, num_items %d, head %d\n[", q->capacity,
-	 q->num_items, q->head);
-  for (i = 0; i < q->capacity; i++) {
-      printf("%s%.2f", i == 0 ? "" : ", ", q->items[i]);
-  }
-  printf("]\n");
+    int i;
+    printf("capacity %d, num_items %d, head %d\n[", q->capacity,
+           q->num_items, q->head);
+    for (i = 0; i < q->capacity; i++) {
+        printf("%s%.2f", i == 0 ? "" : ", ", q->items[i]);
+    }
+    printf("]\n");
 }
 int queue_size(queue q) {
-  return q->num_items;
+    return q->num_items;
 }
 
 
